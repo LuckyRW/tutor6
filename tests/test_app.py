1483f0bd7cb3a6be6d9745366f4900d5b0dc61d7
LuckyRW <luckyrw4@gmail.com>
2025-05-22 16:12:25 +0700

    fjdskfh

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ from app import app
 class TestApp(unittest.TestCase):
     def test_home(self):
         response = app.test_client().get('/')
-        self.assertEqual(response.data, b'Hello, World!')
+        self.assertEqual(response.data, b'Hello, world!')
 
 if __name__ == "__main__":
     unittest.main()
